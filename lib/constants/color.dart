e7a2dea543ab38cea1390860c190fb1cab40b6d9
Abhiyan Khanal <abhiyan5588@gmail.com>
2021-06-20 22:01:04 +0545

    started with all setups

---
+++ lib/constants/color.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//main Color - green
+const Color mainCol = Color(0XFF39b54a);
+//color for texts , dark gray of green shade
+const Color textCol = Color(0XFF666666);
+//secondary color - orange
+const Color secCol = Color(0XFFF2994A);
+//light gray off white
+const Color backCol = Color(0XFFf4f5f7);
+//white
+const Color white = Color(0XFFFFFFFF);
+//card background color
+const Color cardCol = Color(0XFFdcf4dc);
+//light shade of green
+const Color lightGreen = Color(0XFFc5e2c5);
+//catagory product color
+const Color greyCatagory = Color(0xfff4f5f7);
+//button Color
+const Color buttonColor = Color(0xFFF39A4A);
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,58 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:zivacards/constants/color.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: white,
+        appBar: AppBar(
+          title: Text('Ziva Cards'),
+          actions: [
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.person_outlined,
+                  color: white,
+                  size: 25,
+                )),
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.qr_code,
+                  color: white,
+                  size: 25,
+                )),
+          ],
+          elevation: 0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Stack(
+              children: [
+                CachedNetworkImage(
+                  imageUrl:
+                      "https://static.wixstatic.com/media/72c0b2_71ddacaae7af473590d36a0bb72d2292~mv2.jpg/v1/fill/w_1000,h_594,al_c,q_90,usm_0.66_1.00_0.01/72c0b2_71ddacaae7af473590d36a0bb72d2292~mv2.jpg",
+                  height: Get.height * 0.25,
+                  width: Get.width,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 150.0),
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 60,
+                      foregroundImage: CachedNetworkImageProvider(
+                        "https://cdn.cliqueinc.com/posts/280333/celebrity-new-haircut-280333-1619199407294-main.700x0c.jpg",
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
